d528abd15e973d0517586e4e2ef3de49333b2a43
Aneeta Peter <aneetapeteraaa@gmail.com>
2026-03-31 04:52:37 +0530

    fix: pgsql connection reads DB_HOST/DB_DATABASE not DB_PGSQL_* - fixes SQLSTATE 08006 error

--- config/database.php
+++ config/database.php
@@ -85,12 +85,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_PGSQL_URL', env('DB_URL')),
-            'host' => env('DB_PGSQL_HOST', '127.0.0.1'),
-            'port' => env('DB_PGSQL_PORT', '5432'),
-            'database' => env('DB_PGSQL_DATABASE', 'laravel'),
-            'username' => env('DB_PGSQL_USERNAME', 'root'),
-            'password' => env('DB_PGSQL_PASSWORD', ''),
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
